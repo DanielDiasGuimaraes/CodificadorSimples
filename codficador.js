33d41776e9a22bf28eedea12aedb4fe8152b2809
DanielDiasGuimaraes <112535232+DanielDiasGuimaraes@users.noreply.github.com>
2022-09-29 19:20:08 -0300

    Add files via upload

---
+++ codficador.js
@@ -0,0 +1,596 @@
+const la = 'a'
+const lb = 'b'
+const lc = 'c'
+const ld = 'd'
+const le = 'e'
+const lf = 'f'
+const lg = 'g'
+const lh = 'h'
+const li = 'i'
+const lj = 'j'
+const lk = 'k'
+const ll = 'l'
+const lm = 'm'
+const ln = 'n'
+const lo = 'o'
+const lp = 'p'
+const lq = 'q'
+const lr = 'r'
+const ls = 's'
+const lt = 't'
+const lu = 'u'
+const lv = 'v'
+const lw = 'w'
+const ly = 'y'
+const lx = 'x'
+const lz = 'z'
+
+function criptorage ()
+{
+let entrada = document.getElementById('caixatxt').value
+
+if (la == entrada)
+{
+    let resultla1 = document.getElementById('rdc')
+    resultla1.innerText ='1744'
+}
+if (lb == entrada) 
+{
+    let resultla2 = document.getElementById('rdc')
+    resultla2.innerText ='3673'
+}
+if (lc == entrada) 
+{
+    let resultla3 = document.getElementById('rdc')
+    resultla3.innerText = '1133'
+}
+if (ld == entrada) 
+{
+    let resultla4 = document.getElementById('rdc')
+    resultla4.innerText= '1001'
+}
+if (le == entrada)
+{
+    let resultla5 = document.getElementById('rdc')
+    resultla5.innerText = '2642'
+}
+if (lf == entrada)
+{
+    let resultla6 = document.getElementById('rdc')
+    resultla6.innerText = '4418'
+}
+if (lg == entrada)
+{
+    let resultla6 = document.getElementById('rdc')
+    resultla6.innerText = '5594'
+}
+if (lh == entrada)
+{
+    let resultla7 = document.getElementById('rdc')
+    resultla7.innerText = '4561'
+}
+if (li == entrada)
+{
+    let resultla8 = document.getElementById('rdc')
+    resultla8.innerText = '5287'
+}
+if (lj == entrada)
+{
+    let resultla9 = document.getElementById('rdc')
+    resultla9.innerText = '5996'
+}
+if (lk == entrada)
+{
+    let resultla10 = document.getElementById('rdc')
+    resultla10.innerText = '4682'
+}
+if (ll == entrada)
+{
+    let resultla11 = document.getElementById('rdc')
+    resultla11.innerText = '7762'
+}
+if (lm == entrada)
+{
+    let resultla12 = document.getElementById('rdc')
+    resultla12.innerText = '3417'
+}
+if (ln == entrada)
+{
+    let resultla13 = document.getElementById('rdc')
+    resultla13.innerText = '8894'
+}
+if (lo == entrada)
+{
+    let resultla14 = document.getElementById('rdc')
+    resultla14.innerText = '5136'
+}
+if (lp == entrada)
+{
+    let resultla15 = document.getElementById('rdc')
+    resultla15.innerText = '2546'
+}
+if (lq == entrada)
+{
+    let resultla16 = document.getElementById('rdc')
+    resultla16.innerText = '3458'
+}
+if (lr == entrada)
+{
+    let resultla17 = document.getElementById('rdc')
+    resultla17.innerText = '4164'
+}
+if (ls == entrada)
+{
+    let resultla18 = document.getElementById('rdc')
+    resultla18.innerText = '6725'
+}
+if (lt == entrada)
+{
+    let resultla19 = document.getElementById('rdc')
+    resultla19.innerText = '5547'
+}
+if (lu == entrada)
+{
+    let resultla20 = document.getElementById('rdc')
+    resultla20.innerText = '9990'
+}
+if (lv == entrada)
+{
+    let resultla21 = document.getElementById('rdc')
+    resultla21.innerText = '4564'
+}
+if (lw == entrada)
+{
+    let resultla22 = document.getElementById('rdc')
+    resultla22.innerText = '4359'
+}
+if (lx == entrada)
+{
+    let resultla23 = document.getElementById('rdc')
+    resultla23.innerText = '6247'
+}
+if (ly == entrada)
+{
+    let resultla24 = document.getElementById('rdc')
+    resultla24.innerText = '1436'
+}
+if (lz == entrada)
+{
+    let resultla25 = document.getElementById('rdc')
+    resultla25.innerText = '9236'
+}
+}
+
+function criptorage2() {
+let entrada2 = document.getElementById('caixatxt2').value
+
+if (la == entrada2)
+{
+    let resultla1 = document.getElementById('rdc2')
+    resultla1.innerText ='1744'
+}
+if (lb == entrada2) 
+{
+    let resultla2 = document.getElementById('rdc2')
+    resultla2.innerText ='3673'
+}
+if (lc == entrada2) 
+{
+    let resultla3 = document.getElementById('rdc2')
+    resultla3.innerText = '1133'
+}
+if (ld == entrada2) 
+{
+    let resultla4 = document.getElementById('rdc2')
+    resultla4.innerText= '1001'
+}
+if (le == entrada2)
+{
+    let resultla5 = document.getElementById('rdc2')
+    resultla5.innerText = '2642'
+}
+if (lf == entrada2)
+{
+    let resultla6 = document.getElementById('rdc2')
+    resultla6.innerText = '4418'
+}
+if (lg == entrada2)
+{
+    let resultla6 = document.getElementById('rdc2')
+    resultla6.innerText = '5594'
+}
+if (lh == entrada2)
+{
+    let resultla7 = document.getElementById('rdc2')
+    resultla7.innerText = '4561'
+}
+if (li == entrada2)
+{
+    let resultla8 = document.getElementById('rdc2')
+    resultla8.innerText = '5287'
+}
+if (lj == entrada2)
+{
+    let resultla9 = document.getElementById('rdc2')
+    resultla9.innerText = '5996'
+}
+if (lk == entrada2)
+{
+    let resultla10 = document.getElementById('rdc2')
+    resultla10.innerText = '4682'
+}
+if (ll == entrada2)
+{
+    let resultla11 = document.getElementById('rdc2')
+    resultla11.innerText = '7762'
+}
+if (lm == entrada2)
+{
+    let resultla12 = document.getElementById('rdc2')
+    resultla12.innerText = '3417'
+}
+if (ln == entrada2)
+{
+    let resultla13 = document.getElementById('rdc2')
+    resultla13.innerText = '8894'
+}
+if (lo == entrada2)
+{
+    let resultla14 = document.getElementById('rdc2')
+    resultla14.innerText = '5136'
+}
+if (lp == entrada2)
+{
+    let resultla15 = document.getElementById('rdc2')
+    resultla15.innerText = '2546'
+}
+if (lq == entrada2)
+{
+    let resultla16 = document.getElementById('rdc2')
+    resultla16.innerText = '3458'
+}
+if (lr == entrada2)
+{
+    let resultla17 = document.getElementById('rdc2')
+    resultla17.innerText = '4164'
+}
+if (ls == entrada2)
+{
+    let resultla18 = document.getElementById('rdc2')
+    resultla18.innerText = '6725'
+}
+if (lt == entrada2)
+{
+    let resultla19 = document.getElementById('rdc2')
+    resultla19.innerText = '5547'
+}
+if (lu == entrada2)
+{
+    let resultla20 = document.getElementById('rdc2')
+    resultla20.innerText = '9990'
+}
+if (lv == entrada2)
+{
+    let resultla21 = document.getElementById('rdc2')
+    resultla21.innerText = '4564'
+}
+if (lw == entrada2)
+{
+    let resultla22 = document.getElementById('rdc2')
+    resultla22.innerText = '4359'
+}
+if (lx == entrada2)
+{
+    let resultla23 = document.getElementById('rdc2')
+    resultla23.innerText = '6247'
+}
+if (ly == entrada2)
+{
+    let resultla24 = document.getElementById('rdc2')
+    resultla24.innerText = '1436'
+}
+if (lz == entrada2)
+{
+    let resultla25 = document.getElementById('rdc2')
+    resultla25.innerText = '9236'
+}     
+}
+
+function criptorage3() {
+let entrada3 = document.getElementById('caixatxt3').value
+
+if (la == entrada3)
+{
+    let resultla1 = document.getElementById('rdc3')
+    resultla1.innerText ='1744'
+}
+if (lb == entrada3) 
+{
+    let resultla2 = document.getElementById('rdc3')
+    resultla2.innerText ='3673'
+}
+if (lc == entrada3) 
+{
+    let resultla3 = document.getElementById('rdc3')
+    resultla3.innerText = '1133'
+}
+if (ld == entrada3) 
+{
+    let resultla4 = document.getElementById('rdc3')
+    resultla4.innerText= '1001'
+}
+if (le == entrada3)
+{
+    let resultla5 = document.getElementById('rdc3')
+    resultla5.innerText = '2642'
+}
+if (lf == entrada3)
+{
+    let resultla6 = document.getElementById('rdc3')
+    resultla6.innerText = '4418'
+}
+if (lg == entrada3)
+{
+    let resultla6 = document.getElementById('rdc3')
+    resultla6.innerText = '5594'
+}
+if (lh == entrada3)
+{
+    let resultla7 = document.getElementById('rdc3')
+    resultla7.innerText = '4561'
+}
+if (li == entrada3)
+{
+    let resultla8 = document.getElementById('rdc3')
+    resultla8.innerText = '5287'
+}
+if (lj == entrada3)
+{
+    let resultla9 = document.getElementById('rdc3')
+    resultla9.innerText = '5996'
+}
+if (lk == entrada3)
+{
+    let resultla10 = document.getElementById('rdc3')
+    resultla10.innerText = '4682'
+}
+if (ll == entrada3)
+{
+    let resultla11 = document.getElementById('rdc3')
+    resultla11.innerText = '7762'
+}
+if (lm == entrada3)
+{
+    let resultla12 = document.getElementById('rdc3')
+    resultla12.innerText = '3417'
+}
+if (ln == entrada3)
+{
+    let resultla13 = document.getElementById('rdc3')
+    resultla13.innerText = '8894'
+}
+if (lo == entrada3)
+{
+    let resultla14 = document.getElementById('rdc3')
+    resultla14.innerText = '5136'
+}
+if (lp == entrada3)
+{
+    let resultla15 = document.getElementById('rdc3')
+    resultla15.innerText = '2546'
+}
+if (lq == entrada3)
+{
+    let resultla16 = document.getElementById('rdc3')
+    resultla16.innerText = '3458'
+}
+if (lr == entrada3)
+{
+    let resultla17 = document.getElementById('rdc3')
+    resultla17.innerText = '4164'
+}
+if (ls == entrada3)
+{
+    let resultla18 = document.getElementById('rdc3')
+    resultla18.innerText = '6725'
+}
+if (lt == entrada3)
+{
+    let resultla19 = document.getElementById('rdc3')
+    resultla19.innerText = '5547'
+}
+if (lu == entrada3)
+{
+    let resultla20 = document.getElementById('rdc3')
+    resultla20.innerText = '9990'
+}
+if (lv == entrada3)
+{
+    let resultla21 = document.getElementById('rdc3')
+    resultla21.innerText = '4564'
+}
+if (lw == entrada3)
+{
+    let resultla22 = document.getElementById('rdc3')
+    resultla22.innerText = '4359'
+}
+if (lx == entrada3)
+{
+    let resultla23 = document.getElementById('rdc3')
+    resultla23.innerText = '6247'
+}
+if (ly == entrada3)
+{
+    let resultla24 = document.getElementById('rdc3')
+    resultla24.innerText = '1436'
+}
+if (lz == entrada3)
+{
+    let resultla25 = document.getElementById('rdc3')
+    resultla25.innerText = '9236'
+}     
+}
+
+function cropi(){
+
+        const l1 = '1744'
+        const l2 = '3673'
+        const l3 = '1133'
+        const l4 = '1001'
+        const l5 = '2642'
+        const l6 = '4418'
+        const l7 = '5594'
+        const l8 = '4561'
+        const l9 = '5287'
+        const l10 = '5996'
+        const l11 = '4682'
+        const l12 = '7762'
+        const l13 = '3417'
+        const l14 = '8894'
+        const l15 = '5136'
+        const l16 = '2546'
+        const l17 = '3458'
+        const l18 = '4164'
+        const l19 = '6725'
+        const l20 = '5547'
+        const l21 = '9990'
+        const l22 = '4564'
+        const l23 = '4359'
+        const l24 = '6247'
+        const l25 = '1436'
+        const l26 = '9236'
+    
+
+        let txtindeco = document.getElementById('caixatxtdecodificador').value
+
+    if (l1 == txtindeco)
+    {
+        let resultla1 = document.getElementById('rfv')
+        resultla1.innerText ='a'
+    }
+    if (l2 == txtindeco) 
+    {
+        let resultla2 = document.getElementById('rfv')
+        resultla2.innerText ='b'
+    }
+    if (l3 == txtindeco) 
+    {
+        let resultla3 = document.getElementById('rfv')
+        resultla3.innerText = 'c'
+    }
+    if (l4 == txtindeco) 
+    {
+        let resultla4 = document.getElementById('rfv')
+        resultla4.innerText= 'd'
+    }
+    if (l5 == txtindeco)
+    {
+        let resultla5 = document.getElementById('rfv')
+        resultla5.innerText = 'e'
+    }
+    if (l6 == txtindeco)
+    {
+        let resultla6 = document.getElementById('rfv')
+        resultla6.innerText = 'f'
+    }
+    if (l7 == txtindeco)
+    {
+        let resultla6 = document.getElementById('rfv')
+        resultla6.innerText = 'g'
+    }
+    if (l8 == txtindeco)
+    {
+        let resultla7 = document.getElementById('rfv')
+        resultla7.innerText = 'h'
+    }
+    if (l9 == txtindeco)
+    {
+        let resultla8 = document.getElementById('rfv')
+        resultla8.innerText = 'i'
+    }
+    if (l10 == txtindeco)
+    {
+        let resultla9 = document.getElementById('rfv')
+        resultla9.innerText = 'j'
+    }
+    if (l11 == txtindeco)
+    {
+        let resultla10 = document.getElementById('rfv')
+        resultla10.innerText = 'k'
+    }
+    if (l12 == txtindeco)
+    {
+        let resultla11 = document.getElementById('rfv')
+        resultla11.innerText = 'l'
+    }
+    if (l13 == txtindeco)
+    {
+        let resultla12 = document.getElementById('rfv')
+        resultla12.innerText = 'm'
+    }
+    if (l14 == txtindeco)
+    {
+        let resultla13 = document.getElementById('rfv')
+        resultla13.innerText = 'n'
+    }
+    if (l15 == txtindeco)
+    {
+        let resultla14 = document.getElementById('rfv')
+        resultla14.innerText = 'o'
+    }
+    if (l16 == txtindeco)
+    {
+        let resultla15 = document.getElementById('rfv')
+        resultla15.innerText = 'p'
+    }
+    if (l17 == txtindeco)
+    {
+        let resultla16 = document.getElementById('rfv')
+        resultla16.innerText = 'q'
+    }
+    if (l18 == txtindeco)
+    {
+        let resultla17 = document.getElementById('rfv')
+        resultla17.innerText = 'r'
+    }
+    if (l19 == txtindeco)
+    {
+        let resultla18 = document.getElementById('rfv')
+        resultla18.innerText = 's'
+    }
+    if (l20 == txtindeco)
+    {
+        let resultla19 = document.getElementById('rfv')
+        resultla19.innerText = 't'
+    }
+    if (l21 == txtindeco)
+    {
+        let resultla20 = document.getElementById('rfv')
+        resultla20.innerText = 'u'
+    }
+    if (l22 == txtindeco)
+    {
+        let resultla21 = document.getElementById('rfv')
+        resultla21.innerText = 'v'
+    }
+    if (l23 == txtindeco)
+    {
+        let resultla22 = document.getElementById('rfv')
+        resultla22.innerText = 'w'
+    }
+    if (l24 == txtindeco)
+    {
+        let resultla23 = document.getElementById('rfv')
+        resultla23.innerText = 'y'
+    }
+    if (l25 == txtindeco)
+    {
+        let resultla24 = document.getElementById('rfv')
+        resultla24.innerText = 'x'
+    }
+    if (l26 == txtindeco)
+    {
+        let resultla25 = document.getElementById('rfv')
+        resultla25.innerText = 'z'
+    }
+}
